5961b8dbade60ac68a89b9a5a2e90fb33ae55bab
khemraj regmi <khemrajregmi@khemrajs-mbp.localdomain>
2024-04-03 16:03:08 +0200

    added some seeder file to make easy for filter

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,13 +5,16 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class CustomerSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        \App\Models\Customer::factory(25)->create();
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
     }
 }
